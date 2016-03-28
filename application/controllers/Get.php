4d38201cb6ac6b6d774aab237ef592f25994411c
xiaoerge <xiaoerge616@aol.com>
2016-03-28 18:38:58 -0400

    Not Like query method added

--- application/controllers/Get.php
+++ application/controllers/Get.php
@@ -15,7 +15,10 @@ class Get extends REST_Controller {
     public function index_get()
     {
         //no table name, just crash out
-        if (!array_key_exists('table', $this->query())) {
+        $table_key = $this->Get_model->getTableKey();
+        $query_param_key = $this->Get_model->getQueryParamKey();
+
+        if (!array_key_exists($table_key, $this->query())) {
             $this->response('Missing name query parameter', 500);
         }
 
@@ -28,18 +31,19 @@ class Get extends REST_Controller {
          * Order of precedence
          * The order of methods by precedence, a smaller array index is checked before a larger array index.
          * 'all' should always remain at the end to be used as a fallback.
+         * 'all' does not need to be a http parameter from the url.
          */
-        $method_strings = ['where', 'like', 'all'];
+        $method_strings = ['where', 'like', 'not_like', 'all'];
 
-        $table_name = urldecode($this->query('table'));
-        $result = array('table' => $table_name);
+        $table_name = urldecode($this->query($table_key));
+        $result = array($table_key => $table_name);
         $fall_back = true;
 
         foreach ($method_strings as $method) {
             if (array_key_exists($method, $this->query())) {
                 $x_www_form_encoded = urldecode($this->query($method));
                 $query_params = ResourceAccess::BuildParams($x_www_form_encoded);
-                $method_params = ['table' => $table_name, 'query_params' => $query_params];
+                $method_params = [$table_key => $table_name, $query_param_key => $query_params];
                 $result['data'] = $this->Get_model->getResource($method, $method_params);
 
                 $fall_back = false;
@@ -48,7 +52,7 @@ class Get extends REST_Controller {
         }
 
         if ($fall_back) {
-            $result['data'] = $this->Table_model->getResource('all', $method_params = ['name' => $table_name, 'query_params' => []]);
+            $result['data'] = $this->Get_model->getResource('all', $method_params = [$table_key => $table_name, $query_param_key => []]);
         }
 
         $this->response($result);
--- application/models/Get_model.php
+++ application/models/Get_model.php
@@ -16,35 +16,47 @@ class Get_model extends CI_Model {
         parent::__construct();
         $this->methods = [];
 
-        $this->registerAll();
-        $this->registerWhere();
-        $this->registerLike();
+        $this->registerAll('all');
+        $this->registerWhere('where');
+        $this->registerLike('like');
+        $this->registerNotLike('not_like');
     }
 
-    private function registerAll() {
-        $this->methods['all'] = function($method_params) {
-            $table_name = $method_params['table'];
+    private function registerAll($key) {
+        $this->methods[$key] = function($method_params) {
+            $table_name = $method_params[$this->getTableKey()];
             $query = $this->db->get($table_name);
             return $query->result_array();
         };
     }
 
-    private function registerWhere() {
-        $this->methods['where'] = function($method_params) {
-            $table_name = $method_params['table'];
-            $query_params = $method_params['query_params'];
+    private function registerWhere($key) {
+        $this->methods[$key] = function($method_params) {
+            $table_name = $method_params[$this->getTableKey()];
+            $query_params = $method_params[$this->getQueryParamKey()];
             $query = $this->db->get_where($table_name, $query_params);
 
             return $query->result_array();
         };
     }
 
-    private function registerLike() {
-        $this->methods['like'] = function($method_params) {
-            $table_name = $method_params['table'];
-            $query_params = $method_params['query_params'];
+    private function registerLike($key) {
+        $this->methods[$key] = function($method_params) {
+            $table_name = $method_params[$this->getTableKey()];
+            $query_params = $method_params[$this->getQueryParamKey()];
 
             $query = $this->db->like($query_params);
+            $query = $query->get($table_name);
+            return $query->result_array();
+        };
+    }
+
+    private function registerNotLike($key) {
+        $this->methods[$key] = function($method_params) {
+            $table_name = $method_params[$this->getTableKey()];
+            $query_params = $method_params[$this->getQueryParamKey()];
+
+            $query = $this->db->not_like($query_params);
             $query = $query->get($table_name);
             return $query->result_array();
         };
@@ -53,5 +65,13 @@ class Get_model extends CI_Model {
     public function getResource($method = 'all', $method_params = []) {
         $func = $this->methods[$method];
         return $func($method_params);
+    }
+
+    public function getTableKey() {
+        return 'table';
+    }
+
+    public function getQueryParamKey() {
+        return 'query_params';
     }
 }
